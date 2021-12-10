f6444cda4fd072f115f5298ec3f147438721c8f0
AiRikux <nabilahqanuwar@gmail.com>
2021-12-10 22:38:46 +0700

    create function for hash code and index and method of assigning
    Signed-off-by: AiRikux <nabilahqanuwar@gmail.com>

--- CS102/hash_maps.py
+++ CS102/hash_maps.py
@@ -1,4 +1,24 @@
 class HashMap:
-  def __init__(self, array_size):
-    self.array_size = array_size
-    self.array = [None]*array_size
+	def __init__(self, array_size):
+		self.array_size = array_size
+		self.array = [None] * array_size
+
+	def hash(self, key):
+		# turn the key into list of bytes
+		key_bytes = key.encode()
+		# turn the bytes into hash code with sum
+		hash_code = sum(key_bytes)
+		return hash_code
+
+	def compressor(self, hash_code):
+		# get modulus
+		return hash_code % self.array_size
+
+	def assign(self, key, value):
+		# get hash code
+		code = self.hash(key)
+		# get index
+		index = self.compressor(code)
+		# assign value to this index
+		self.array[index] = value
+
