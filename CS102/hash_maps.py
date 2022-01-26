21bef0ca811b42b0eb03829153098d57186e82ea
AiRikux <nabilahqanuwar@gmail.com>
2022-01-26 23:56:51 +0700

    using open addressing scheme in the setter
    Signed-off-by: AiRikux <nabilahqanuwar@gmail.com>

--- CS102/hash_maps.py
+++ CS102/hash_maps.py
@@ -1,31 +1,61 @@
 class HashMap:
 	def __init__(self, array_size):
 		self.array_size = array_size
-		self.array = [None] * array_size
+		self.array = [None for item in range(array_size)]
 
-	def hash(self, key):
-		# turn the key into list of bytes
+	def hash(self, key, count_collisions=0):
 		key_bytes = key.encode()
-		# turn the bytes into hash code with sum
 		hash_code = sum(key_bytes)
-		return hash_code
+		return hash_code + count_collisions
 
 	def compressor(self, hash_code):
-		# get modulus
 		return hash_code % self.array_size
 
 	def assign(self, key, value):
-		# get hash code
-		code = self.hash(key)
-		# get index
-		index = self.compressor(code)
-		# assign value to this index
-		self.array[index] = value
+		array_index = self.compressor(self.hash(key))
+		current_array_value = self.array[array_index]
+
+		if current_array_value is None:
+			self.array[array_index] = [key, value]
+			return
+
+		if current_array_value[0] == key:
+			self.array[array_index] = [key, value]
+			return
+
+		# Collision
+
+		number_collisions = 1
+
+		while (current_array_value[0] != key):
+			new_hash_code = self.hash(key, number_collisions)
+			new_array_index = self.compressor(new_hash_code)
+			current_array_value = self.array[new_array_index]
+
+			if current_array_value is None:
+				self.array[new_array_index] = [key, value]
+				return
+
+			if current_array_value[0] == key:
+				self.array[new_array_index] = [key, value]
+				return
+
+		number_collisions += 1
+
+		return
 
 	def retrieve(self, key):
-		# get index with same way as assign
 		array_index = self.compressor(self.hash(key))
-		return self.array[array_index]
+		possible_return_value = self.array[array_index]
+
+		if possible_return_value is None:
+			return None
+
+		if possible_return_value[0] == key:
+			return possible_return_value[1]
+
+		# possible_return_value holds different key
+		return
 
 # create the object
 hash_map = HashMap(20)
